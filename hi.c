8d13bf4ab7ef9915c8203f442acc1ab1ad6554ab
saitejaswiniteju <99312930+saitejaswiniteju@users.noreply.github.com>
2022-02-09 17:30:35 -1000

    Create hi.c

---
+++ hi.c
@@ -0,0 +1,9 @@
+num1=int(input("Enter first Number"))
+num2=int(input ("Enter second Number"))
+if(num1 >=num2):
+if(num1 == num2):
+print("Both numbers are equal.")
+else:
+print("First number is bigger than the second number.")
+else:
+print("second number is bigger than the first number.")
